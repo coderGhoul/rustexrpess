40d0467cfb7b15893be81b6b2ed7bd192cb83f50
coderGhoul <1773110186@qq.com>
2022-11-13 11:35:46 +0800

    while

--- src/main.rs
+++ src/main.rs
@@ -109,21 +109,28 @@ fn main(){
     // println!("{}",result)
 
     //循环标签以消除多个循环之间的歧义
-    let mut count = 0;
-    'counting_up:loop {
-        println!("count = {count}");
-        let mut remaining =10;
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -=1;
-        }
-        count +=1;
+    // let mut count = 0;
+    // 'counting_up:loop {
+    //     println!("count = {count}");
+    //     let mut remaining =10;
+    //     loop {
+    //         println!("remaining = {remaining}");
+    //         if remaining == 9 {
+    //             break;
+    //         }
+    //         if count == 2 {
+    //             break 'counting_up;
+    //         }
+    //         remaining -=1;
+    //     }
+    //     count +=1;
+    // }
+    // println!("End count = {count}")
+    //条件while语句
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}");
+        number -=1
     }
-    println!("End count = {count}")
+    println!("LIFTOFF!")
 }
